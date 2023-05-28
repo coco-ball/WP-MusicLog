64d52295572a5df57939978f5f3cba8bd523ffa5
coco-ball <kidbean02@snu.ac.kr>
2023-05-28 16:24:23 +0900

    main page - toggle 구현

--- music-log/src/components/MainPage.js
+++ music-log/src/components/MainPage.js
@@ -1,41 +1,70 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 import PostLog from "./PostLog.js";
 import MusicLog from "./MusicLog.js";
 
-
 const MainPage = () => {
-    const [stateVar, setStateVar] = useState(0);
+  const [stateVar, setStateVar] = useState(0);
 
-    function toggleStateVar() {
-        setStateVar(prevState => (prevState === 0 ? 1 : 0));
-    }
+  function toggleStateVar() {
+    setStateVar((prevState) => (prevState === 0 ? 1 : 0));
+  }
 
-    return (
-        <div className = "container">
-            <div className = "text-center">
-                <h2>music log demo</h2>
-                <button className={`w-40
-                      justify-self-center
-                      p-1 mb-4
-                      bg-rose-500 text-white
-                      border border-rose-500 rounded
-                    hover:bg-white hover:text-rose-500`}
-                    onClick={() => toggleStateVar()}>toggle</button>
-            </div>
-            <div className="contets">
-                {stateVar === 0 ? (
-                    <div className="post">
-                        <PostLog />
-                    </div>
-                ) : (
-                    <div className="check">
-                        <MusicLog />
-                    </div>
-                )}
-            </div>
-        </div>
-    )
-}
+  return (
+    // <div className = "container">
+    //     <div className = "text-center">
+    //         <h2>music log demo</h2>
+    //         <button className={`w-40
+    //               justify-self-center
+    //               p-1 mb-4
+    //               bg-rose-500 text-white
+    //               border border-rose-500 rounded
+    //             hover:bg-white hover:text-rose-500`}
+    //             onClick={() => toggleStateVar()}>toggle</button>
+    //     </div>
+    //     <div className="contents">
+    //         {stateVar === 0 ? (
+    //             <div className="post">
+    //                 <PostLog />
+    //             </div>
+    //         ) : (
+    //             <div className="check">
+    //                 <MusicLog />
+    //             </div>
+    //         )}
+    //     </div>
+    // </div>
+
+    <div className="w-screen px-64 flex-col justify-center">
+      <div className="flex mt-20">
+        <img
+          src={stateVar === 0 ? "/write.svg" : "album.svg"}
+          alt=""
+          className="mr-4"
+        ></img>
+        <h1 className="text-5xl font-bold">
+          {stateVar === 0 ? "음악 로그 작성" : "00님의 음악로그"}
+        </h1>
+        <button
+          className={`w-30 justify-self-center px-5 py-2 ml-auto text-3xl rounded bg-gray-300 hover:bg-gray-400`}
+          onClick={() => toggleStateVar()}
+        >
+          {stateVar === 0 ? "음악 로그 보기" : "로그 작성하기"}
+        </button>
+      </div>
+      <div className="contents">
+        {stateVar === 0 ? (
+          <div className="post">
+            <PostLog />
+          </div>
+        ) : (
+          <div className="check">
+            <MusicLog />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default MainPage;
--- music-log/src/components/PostLog.js
+++ music-log/src/components/PostLog.js
@@ -1,7 +1,26 @@
 import { useEffect, useState } from "react";
+//import MusicLog from "@/src/components/MusicLog";
+
+// firebase 관련 모듈을 불러옵니다.
+/*
+import { db } from "@/firebase";
+import {
+  collection,
+  query,
+  doc,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  orderBy,
+  where
+} from "firebase/firestore";
+*/
+
+// DB의 todos 컬렉션 참조를 만듭니다. 컬렉션 사용시 잘못된 컬렉션 이름 사용을 방지합니다.
+//const postlogCollection = collection(db, "postlogs");
 
 const PostLog = () => {
-  console.log("postlog");
   const [input, setInput] = useState("");
 
   const date = new Date().toISOString().substring(0, 10);
@@ -16,40 +35,64 @@ const PostLog = () => {
     text: "text-input",
   };
 
+  // firebase 관련 명령
+  //albumCover 엘범 커버 사진은 데이터에 포함하지 않나요?
+  //spotify API연동-->userid, 노래 title, artist, album cover.. 불러오기 -->
+
   return (
-    <div className="w-screen px-64 flex-col justify-center">
-      <div className="flex mt-20">
-        <img src="/write.svg" alt="" className="mr-4"></img>
-        <h1 className="text-5xl font-bold">음악 로그 작성</h1>
-        <button className="w-30 justify-self-center px-5 py-2 ml-auto text-3xl rounded bg-gray-300 hover:bg-gray-400">
-          저장하기
+    <body className="w-auto flex mt-8">
+      <div className="w-72 mr-4 bg-white rounded p-4">
+        <img className="w-auto mb-4 rounded" src="/albumCover.jpeg"></img>
+        <p className="text-center text-3xl mb-1">{exData.title}</p>
+        <p className="text-center text-2xl">{exData.artist}</p>
+      </div>
+      <div className="w-full bg-white rounded p-4">
+        <p className="text-2xl font-bold mb-1">지금 어디에 계시나요?</p>
+        <p className="mb-4">{exData.location}</p>
+        <p className="text-2xl font-bold mb-1">시간</p>
+        <p className="mb-4">{exData.datetime}</p>
+        <label htmlFor="input-text" className="text-2xl font-bold">
+          지금 뭐하고 계시나요? 간단한 메모를 남겨주세요.
+        </label>
+        <textarea
+          id="input-text"
+          type="text"
+          className="w-full h-48 p-1 mt-2 rounded bg-gray-200 focus:outline-none focus:bg-gray-300"
+          value={input}
+          onChange={(e) => useEffect(() => setInput(e.target.value))}
+        ></textarea>
+        <button
+          className={`w-30 justify-self-center px-5 py-2 ml-auto text-3xl rounded bg-gray-300 hover:bg-gray-400`}
+          // onClick={() => toggleStateVar()}
+        >
+          작성 완료
         </button>
       </div>
-      <body className="w-auto flex mt-8">
-        <div className="w-72 mr-4 bg-white rounded p-4">
-          <img className="w-auto mb-4 rounded" src="/albumCover.jpeg"></img>
-          <p className="text-center text-3xl mb-1">{exData.title}</p>
-          <p className="text-center text-2xl">{exData.artist}</p>
-        </div>
-        <div className="w-full bg-white rounded p-4">
-          <p className="text-2xl font-bold mb-1">지금 어디에 계시나요?</p>
-          <p className="mb-4">{exData.location}</p>
-          <p className="text-2xl font-bold mb-1">시간</p>
-          <p className="mb-4">{exData.datetime}</p>
-          <label htmlFor="input-text" className="text-2xl font-bold">
-            지금 뭐하고 계시나요? 간단한 메모를 남겨주세요.
-          </label>
-          <textarea
-            id="input-text"
-            type="text"
-            className="w-full h-48 p-1 mt-2 rounded bg-gray-200 focus:outline-none focus:bg-gray-300"
-            value={input}
-            onChange={(e) => useEffect(() => setInput(e.target.value))}
-          ></textarea>
-        </div>
-      </body>
-    </div>
+    </body>
   );
 };
 
 export default PostLog;
+
+//데이터 저장하는 함수 지정한 후 맨 마지막에 alert('오늘의 음악 로그가 저장되었습니다.'); 코드 추가해서 사용자가 자신의 코드가 저장됨을 알게 하면 좋을 것 같아요
+// 저장하기 버튼 클릭 시 데이터 저장하는 함수
+/*
+function saveData() {
+  const userId = exData.getElementById(' ').textContent;
+  const location = document.getElementById('  ').value;
+  const datetime = document.getElementById('').value;
+  const title = document.getElementById(' ').textContent;
+  const artist = document.getElementById(' ').textContent;
+  /*const albumCover = exData.getElementById(' ').src;*/
+//const text = document.getElementById('text-input').value;
+
+// 여기서 데이터를 데이터베이스에 저장하는 로직을 추가-firebase 연동?
+// 데이터베이스에 저장되는 데이터는 userid, location, datetime, title, artist, text.. *앨범커버도 저장해야?
+
+//alert('오늘의 음악 로그가 저장되었습니다.');
+//}
+
+// 저장하기 버튼에 이벤트 리스너 추가
+//const saveButton = document.getElementById('save-button');
+//saveButton.addEventListener('click', saveData);
+//)
