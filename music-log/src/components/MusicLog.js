583f983ab1b835510770406959ae99fb3e097eff
YunjiCho <76531885+YunjiCho@users.noreply.github.com>
2023-05-30 11:47:50 +0900

    create firebase in musiclog

--- music-log/src/components/MusicLog.js
+++ music-log/src/components/MusicLog.js
@@ -1,30 +1,67 @@
 import { useState, useEffect } from "react";
 import PostLog from "@/src/components/PostLog";
 
+//firebase 관련 모듈을 불러옵니다.
+import { db } from "./firebase";
+import {
+  collection,
+  query,
+  doc,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  orderBy,
+  where,
+} from "firebase/firestore";
+
+// // DB의 postlogs 컬렉션 참조를 만듭니다. 컬렉션 사용시 잘못된 컬렉션 이름 사용을 방지합니다.
+const postlogCollection = collection(db, "postlogs");
+
 const MusicLog = () => {
   const [logs, setLogs] = useState([]);
 
-  useEffect(() => {
-    // Simulating fetching logs from an API or storage
-    const fetchedLogs = [
-      {
-        title: "첫 번째 기록",
-        location: "서울",
-        artist: "아티스트 1",
-        datetime: "2023-05-27 10:00",
-        text: "첫 번째 기록입니다.",
-      },
-      {
-        title: "두 번째 기록",
-        location: "부산",
-        artist: "아티스트 2",
-        datetime: "2023-05-28 15:30",
-        text: "두 번째 기록입니다.",
-      },
-      // Add more logs if needed
-    ];
+  // Firebase에서 불러오는 함수
+  const getLogs = async () => {
+    const q = query(postlogCollection);
 
-    setLogs(fetchedLogs);
+    // Firestore에서 불러오기
+    const results = await getDocs(q);
+    const newLogs = [];
+
+    // 가져온 목록을 newLogs 배열에 담습니다.
+    results.docs.forEach((doc) => {
+      //results에 저장된 데이터를 newTodos 배열에 담습니다.
+      newLogs.push({ ...doc.data() });
+    });
+
+    setLogs(newLogs);
+  };
+
+  useEffect(() => {
+    getLogs();
+
+    //simulating 내용은 주석처리 하였습니다.
+    // Simulating fetching logs from an API or storage
+    // const fetchedLogs = [
+    //   {
+    //     title: "첫 번째 기록",
+    //     location: "서울",
+    //     artist: "아티스트 1",
+    //     datetime: "2023-05-27 10:00",
+    //     text: "첫 번째 기록입니다.",
+    //   },
+    //   {
+    //     title: "두 번째 기록",
+    //     location: "부산",
+    //     artist: "아티스트 2",
+    //     datetime: "2023-05-28 15:30",
+    //     text: "두 번째 기록입니다.",
+    //   },
+    //   // Add more logs if needed
+    // ];
+
+    // setLogs(fetchedLogs);
   }, []);
 
   return (
