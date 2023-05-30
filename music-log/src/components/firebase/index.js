cd3b72fd088a8493276a2d0e99d3af1d448b2443
YunjiCho <76531885+YunjiCho@users.noreply.github.com>
2023-05-30 11:52:39 +0900

    Update index.js

--- music-log/src/components/firebase/index.js
+++ music-log/src/components/firebase/index.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqg3MweI0LFD4Oomq-oCV8dPf5cNN2JS0",
-  authDomain: "musiclog-8ac3e.firebaseapp.com",
-  projectId: "musiclog-8ac3e",
-  storageBucket: "musiclog-8ac3e.appspot.com",
-  messagingSenderId: "532635667537",
-  appId: "1:532635667537:web:3b868b67b22bfb93253c70",
-  measurementId: "G-VD0VCTVLZG",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
